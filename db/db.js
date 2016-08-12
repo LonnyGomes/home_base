1a81abd0d151167d2756527ea8f9e61a24c97cb4
Lonny Gomes <code@lonnygomes.com>
2016-08-11 23:53:21 -0400

    Added database module to interface with mongo db

---
+++ db/db.js
@@ -0,0 +1,58 @@
+/*jslint node: true */
+/*global q */
+var mongoose = require('mongoose');
+var Q = require('q');
+var db;
+
+var tempDeviceSchema = mongoose.Schema({
+    deviceName: String,
+    temperature: Number,
+    humidity: Number
+});
+
+var TemperatureDevice = mongoose.model('TemperatureDevice', tempDeviceSchema);
+
+function saveTemp(data) {
+    'use strict';
+
+    var defer = Q.defer(),
+        td = new TemperatureDevice(data);
+
+    td.save(function (err, d) {
+        if (err) {
+            defer.reject(err);
+        } else {
+            defer.resolve(d);
+        }
+    });
+
+    return defer.promise;
+}
+
+function init(dbUrl) {
+    'use strict';
+
+    var defer = Q.defer();
+
+    //'mongodb://localhost/test'
+    mongoose.connect(dbUrl);
+
+    db = mongoose.connection;
+
+    db.on('error', function (err) {
+        defer.reject(err);
+    });
+
+    db.once('open', function () {
+        defer.resolve(db);
+    });
+
+    return {
+        connect: function () {
+            return defer.promise;
+        },
+        saveTemp: saveTemp
+    };
+}
+
+module.exports = init;
